f8bcdf72c6fbfb7c8399eb4a8d6e8455673a7972
BrunoMendez <bruno.m.275@gmail.com>
2019-03-26 17:41:07 -0600

    map tab functional

--- app/src/main/java/com/itesm/cartelera_tec_mty/MapTab.kt
+++ app/src/main/java/com/itesm/cartelera_tec_mty/MapTab.kt
@@ -1,36 +1,53 @@
 package com.itesm.cartelera_tec_mty
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import com.google.android.gms.maps.model.LatLng
 import android.view.ViewGroup
+
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.CameraPosition
 
-class MapTab : Fragment(), OnMapReadyCallback {
+
+
+class MapTab : SupportMapFragment(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater?.inflate(R.layout.map_tab, container, false)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = childFragmentManager.
+                findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+        return view
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        val currentPlace = CameraPosition.Builder()
+                .target(LatLng(25.651115, -100.289370))
+                .bearing(48f).tilt(0f).zoom(16.9f).build()
+        val tec = LatLng(25.6508008, -100.2891634)
+        mMap.addMarker(MarkerOptions().position(tec).title("Marker in Tec"))
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(currentPlace))
+        println(mMap.cameraPosition)
+
     }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.map_tab, container, false)
-        val mapFragment = rootView.findViewById<MapView>(R.id.mapView)
-
-        mapFragment.getMapAsync(this)
-        return rootView
-    }
-
 }
