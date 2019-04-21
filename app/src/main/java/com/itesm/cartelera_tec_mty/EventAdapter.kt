9d821b0c0d8afa51c59aba48e27a113195154109
omarivanfq <omarivanfq@gmail.com>
2019-04-20 22:44:22 -0500

    EventViewHolder on EventAdapter

--- app/src/main/java/com/itesm/cartelera_tec_mty/EventAdapter.kt
+++ app/src/main/java/com/itesm/cartelera_tec_mty/EventAdapter.kt
@@ -12,16 +12,37 @@ import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
 import java.text.SimpleDateFormat
 
 class EventAdapter (val context: Context, val list:MutableList<Event>):BaseAdapter() {
 
+    /*
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val inflater: LayoutInflater =
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(R.layout.event_row, parent, false)
         bind(view, list[position])
         return view
+    }
+*/
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val eventHolder:EventViewHolder
+        val rowView:View
+        val event:Event = getItem(position) as Event
+
+        if (convertView == null) {
+            val inflater: LayoutInflater =
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            rowView = inflater.inflate(R.layout.event_row, parent, false)
+            eventHolder = EventViewHolder(rowView)
+            rowView.tag = eventHolder
+        } else {
+            rowView = convertView
+            eventHolder = convertView.tag as EventViewHolder
+        }
+        eventHolder.bind(event)
+        return rowView
     }
 
     override fun getItem(position: Int): Any {
@@ -36,6 +57,7 @@ class EventAdapter (val context: Context, val list:MutableList<Event>):BaseAdapt
         return list.size
     }
 
+    /*
     fun bind(containerView: View, event: Event) {
         val textviewTitle = containerView.findViewById<TextView>(R.id.textview_title)
         val textviewLocation = containerView.findViewById<TextView>(R.id.textview_location)
@@ -52,6 +74,26 @@ class EventAdapter (val context: Context, val list:MutableList<Event>):BaseAdapt
             val detailIntent = Intent(context, EventDetail::class.java)
             detailIntent.putExtra(EventsTab.EXTRA_EVENT, event)
             startActivity(context, detailIntent, null)
+        }
+    }*/
+
+    inner class EventViewHolder(override val containerView: View) : LayoutContainer {
+        fun bind(event: Event) {
+            val textviewTitle = containerView.findViewById<TextView>(R.id.textview_title)
+            val textviewLocation = containerView.findViewById<TextView>(R.id.textview_location)
+            val textviewDate = containerView.findViewById<TextView>(R.id.textview_date)
+            val textviewTime = containerView.findViewById<TextView>(R.id.textview_time)
+            val imageview = containerView.findViewById<ImageView>(R.id.image_event)
+            textviewTitle.text = event.name //textview_nombre.text = libro.nombre
+            textviewLocation.text = event.location
+            textviewDate.text = TimeFormat.getDate(event.startDateTime)
+            textviewTime.text = TimeFormat.getTime(event.startDateTime) + " - " + TimeFormat.getTime(event.endDateTime)
+            Picasso.get().load(event.photo).into(imageview)
+            containerView.setOnClickListener { _ ->
+                val detailIntent = Intent(context, EventDetail::class.java)
+                detailIntent.putExtra(EventsTab.EXTRA_EVENT, event)
+                startActivity(context, detailIntent, null)
+            }
         }
     }
 
